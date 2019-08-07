80d00ae774c6b7d6bfe6cb8d285be03723eacdc7
Kristina <goncharenkokatja@gmail.com>
2019-08-07 20:34:59 +0200

    add visualisation

--- krioliin_vizualizer/scripts.js
+++ krioliin_vizualizer/scripts.js
@@ -4,8 +4,11 @@ $(document).ready(function()
 	var		start_pos;
 	var		map_x;
 	var		map_y;
-	var		RUN_GAME;
+	var		score_player_o;
+	var		score_player_x;
 
+	score_player_o = 0;
+	score_player_x = 0;
 	$("#button").click(function(){
 		map = $('#map_src').val();
 	});
@@ -13,6 +16,7 @@ $(document).ready(function()
 	function init_map_size()
 	{
 		var		size_pos;
+		var		result;
 	
 		size_pos = map.indexOf("Plateau ");
 		map_y = map.charAt(size_pos + "Plateau ".length);
@@ -24,7 +28,8 @@ $(document).ready(function()
 		map_x = map.charAt(size_pos + "Plateau ".length + 3);
 		map_x += map.charAt(size_pos + "Plateau ".length + 4);
 
-		$("p").append(" X " + map_x + " Y " + map_y);
+		result = map_x + map_y;
+		$("#paragraph").append(" X " + map_x + " Y " + map_y + "result: " + result + "<br>");
 	}
 
 	function set_speed()
@@ -33,20 +38,11 @@ $(document).ready(function()
 	
 		speed = $("input[name=choose_speed]:checked", "#map_src_form").val();
 		if (String(speed) == "speed_slow")
-		{
-			$('p').append("1:" + speed);
 			return (900);
-		}
 		if (String(speed) == "speed_fast")
-		{
-			$('p').append("2:" + speed);
 			return (100);
-		}
 		if (String(speed) == "speed_super_fast")
-		{
-			$('p').append("3:" + speed);
 			return (30);
-		}
 		return (300);
 	}
 
@@ -62,19 +58,20 @@ $(document).ready(function()
 		}
 	}
 
-	function add_move_player_y(x, y)
+	function add_move_player_x(x, y)
 	{
 		var		spot;
 		var		spot_pos;
 	
+		//core_player_x += 1;
 		spot = "#grid div:nth-child(";
 		if (0 < y)
 		{
 			spot_pos = x + (y * map_x);
-			$(spot + spot_pos + ')').addClass("player_one");
+			$(spot + spot_pos + ')').addClass("player_x");
 		}
 		else
-			$(spot + x + ')').addClass("player_one");
+			$(spot + x + ')').addClass("player_x");
 	}
 
 	function add_move_player_o(x, y, character)
@@ -84,10 +81,7 @@ $(document).ready(function()
 		var		class_name;
 
 		spot = "#grid div:nth-child(";
-		//if (character == 'O')
-			class_name = "player_o";
-		// else
-		// 	class_name = "new_figure_o";
+		class_name = "player_o";
 		if (0 < y)
 		{
 			spot_pos = x + (y * map_x);
@@ -111,7 +105,7 @@ $(document).ready(function()
 				if (map_arr[y][x] == "O")
 					add_move_player_o(x, y, map_arr[y][x]);
 				if (map_arr[y][x] == "X")
-					add_move_player_y(x, y);
+					add_move_player_x(x, y);
 				x++;
 			}
 			x = 0;
@@ -122,13 +116,29 @@ $(document).ready(function()
 	function move_start_pos()
 	{
 		start_pos = map.indexOf("000", Number(start_pos));
-		$("p").append("start_pos" + start_pos + "<br>");
-		if (Number(start_pos) == -1)
+	}
+
+	function add_score()
+	{
+		var		got_player_o;
+		var		got_player_x;
+	
+		got_player_o = map.indexOf("got (O)", Number(start_pos));
+		got_player_x = map.indexOf("got (X)", Number(start_pos));
+		if (got_player_o == -1 && got_player_x == -1)
+			return (false);
+		if (Number(got_player_o) < Number(got_player_x) && (Number(got_player_o) != -1))
 		{
-			$("p").append("Minus found" + start_pos + "<br>");
-			Number(RUN_GAME) = -1;
-			
+			score_player_o++;
+			$(".score_player_o").text("Scores player O: " + score_player_o);
 		}
+		else
+		{
+			score_player_x++;
+			$(".score_player_x").text("Scores player X: " + score_player_x);
+		}
+		got_player_o = 0;
+		got_player_x = 0;
 	}
 
 	function fill_one_map_piece(map_arr)
@@ -138,15 +148,13 @@ $(document).ready(function()
 
 		i = 0;
 		start = Number(start_pos) + 3;
-		//$("#paragraph").append("<br>" + "START POSITION 2: " + start_pos);
 		while (i < Number(map_y))
 		{
-		//	$("#paragraph").append("start: " + start + " i: " + i + "<br>");
-			map_arr[i] = map.substring(start, Number(start) + Number(map_x));
+			map_arr[i] = map.substring(start, Number(start) + Number(map_x) + 1);
 			start += Number(map_x) + 5;
 			i++;
 		}
-		//setTimeout(add_move, 900*i, map_arr);
+		add_score();
 		start_pos = start;
 	}
 
@@ -157,24 +165,17 @@ $(document).ready(function()
 		var		i;
 
 		i = 0;
-		RUN_GAME = 1;
 		init_map_size();
 		creat_grid_from_div();
 		speed = set_speed();
 		start_pos = map.indexOf("000");
-		while (Number(RUN_GAME) != -1)
+		while (i < 500)
 		{
 			setTimeout(fill_one_map_piece, Number(speed) * i, map_arr);
 			setTimeout(add_move, Number(speed) * i, map_arr);
 			setTimeout(move_start_pos, Number(speed) * i);
-			if (Number(RUN_GAME) == -1)
-			{
-				$("p").append(" The game is over!");
-				break ;
-			}
+		//	$("p").append("Minus found" + start_pos + " RUN_GAME: " + RUN_GAME+ "<br>");
 			i++;
 		}
 	});
-
-	init_map_size();
 });
